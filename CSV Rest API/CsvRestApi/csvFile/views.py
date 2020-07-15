23f43e005454d2fa36086280ee9b100b02a48658
Deepal11 <64477392+Deepal11@users.noreply.github.com>
2020-07-15 19:05:14 +0530

    Update views.py

--- CSV Rest API/CsvRestApi/csvFile/views.py
+++ CSV Rest API/CsvRestApi/csvFile/views.py
@@ -1,5 +1,3 @@
-#import csv
-#from django.http import HttpResponse
 from rest_framework import generics
 from .models import Employee
 from .serializers import EmployeeSerializer
@@ -8,22 +6,3 @@ from .serializers import EmployeeSerializer
 class ViewList(generics.ListCreateAPIView):
     queryset = Employee.objects.all()
     serializer_class = EmployeeSerializer
-    '''
-    def get_csv(self):
-
-        queryset = Employee.objects.all()
-
-        response = HttpResponse(content_type='text/csv')
-        response['Content-Disposition'] = 'attachment; filename="export.csv"'
-
-        serializer = EmployeeSerializer(queryset, many=True)
-
-        header = EmployeeSerializer.Meta.fields
-
-        writer = csv.DictWriter(response, fieldnames=header)
-        writer.writeheader()
-        for row in serializer.data:
-            writer.writerow(row)
-
-        return response
-    '''
